918157e51115e9051e6257763cb2cabcb24c8821
Joel Anderson <joelbryceanderson@gmail.com>
2017-03-01 12:31:56 -0500

    added selection of colors, saving of chosen color

--- app/src/main/java/abcmeasurecorp/com/measureit/Activities/MainActivity.java
+++ app/src/main/java/abcmeasurecorp/com/measureit/Activities/MainActivity.java
@@ -2,13 +2,22 @@ package abcmeasurecorp.com.measureit.activities;
 
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import java.util.Random;
@@ -27,16 +36,18 @@ public class MainActivity extends AppCompatActivity {
 
     private AppCompatButton mUnitsButton;
     private AppCompatButton mTogglePointerButton;
+    private AlertDialog mColorDialog;
     private RulerView mRulerView;
     private LinearLayout mRightContainer;
-    private SharedPreferences prefs;
+    private SharedPreferences mPrefs;
+    private int mCurrentColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        prefs = getPreferences(MODE_PRIVATE);
+        mPrefs = getPreferences(MODE_PRIVATE);
 
         mRulerView = (RulerView) findViewById(R.id.ruler);
         mRightContainer = (LinearLayout) findViewById(R.id.right_container);
@@ -45,18 +56,22 @@ public class MainActivity extends AppCompatActivity {
         mUnitsButton = (AppCompatButton) findViewById(R.id.toggle_metric_button);
 
         mTogglePointerButton.setOnClickListener(clickTogglePointer());
-        randomColorButton.setOnClickListener(clickRandomColor());
+        randomColorButton.setOnClickListener(clickShowDialog());
         mUnitsButton.setOnClickListener(clickToggleUnits());
 
         initUserPreferences();
     }
 
     private void initUserPreferences() {
-        boolean isMetric = prefs.getBoolean(getString(R.string.ruler_is_metric_pref_key), false);
-        boolean showPointer = prefs.getBoolean(getString(R.string.ruler_show_pointer_pref_key), true);
+        boolean isMetric = mPrefs.getBoolean(getString(R.string.ruler_is_metric_pref_key), false);
+        boolean showPointer = mPrefs.getBoolean(getString(R.string.ruler_show_pointer_pref_key), true);
+        mCurrentColor = mPrefs.getInt(getString(R.string.ruler_color_pref_key),
+                ContextCompat.getColor(MainActivity.this, R.color.colorAccent));
 
         mRulerView.setShowPointer(showPointer);
         mRulerView.setIsMetric(isMetric);
+        mRulerView.setAccentColor(mCurrentColor);
+        mRightContainer.setBackgroundColor(mCurrentColor);
 
         mUnitsButton.setText(isMetric ?
                 getString(R.string.button_imperial) : getString(R.string.button_metric));
@@ -92,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void toggleUnits() {
         String label;
-        SharedPreferences.Editor editor = prefs.edit();
+        SharedPreferences.Editor editor = mPrefs.edit();
         if (mRulerView.isMetric()) {
             label = getString(R.string.button_metric);
             editor.putBoolean(getString(R.string.ruler_is_metric_pref_key), false);
@@ -125,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void togglePointer() {
         String label;
-        SharedPreferences.Editor editor = prefs.edit();
+        SharedPreferences.Editor editor = mPrefs.edit();
         if (mRulerView.isPointerShown()) {
             label = getString(R.string.button_show_pointer);
             editor.putBoolean(getString(R.string.ruler_show_pointer_pref_key), false);
@@ -144,11 +159,21 @@ public class MainActivity extends AppCompatActivity {
      *
      * @return OnClickListener to assign to the toggle button
      */
-    private View.OnClickListener clickRandomColor() {
+    private View.OnClickListener clickShowDialog() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                showDialog();
+            }
+        };
+    }
+
+    private DialogInterface.OnClickListener clickRandomColor() {
+        return new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
                 setRandomColor();
+                saveColorSelection();
             }
         };
     }
@@ -158,10 +183,10 @@ public class MainActivity extends AppCompatActivity {
      */
     private void setRandomColor() {
         Random rnd = new Random();
-        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        mCurrentColor = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
 
-        animateBackgroundColor(color);
-        mRulerView.animateAccentColor(color);
+        animateBackgroundColor(mCurrentColor);
+        mRulerView.animateAccentColor(mCurrentColor);
     }
 
     /**
@@ -192,4 +217,87 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void showDialog() {
+        mCurrentColor = ContextCompat.getColor(MainActivity.this, R.color.colorAccent);
+
+        mColorDialog = new AlertDialog.Builder(this)
+                .setView(R.layout.color_picker)
+                .setTitle(R.string.color_picker_title)
+                .setNegativeButton(R.string.cancel, null)
+                .setNeutralButton(R.string.random_color, clickRandomColor())
+                .show();
+
+        final ImageView button = (ImageView) mColorDialog.findViewById(R.id.choose_color);
+        final ImageView colorSpectrum = (ImageView) mColorDialog.findViewById(R.id.color_spectrum);
+
+        if (colorSpectrum != null && button != null) {
+            final Bitmap bitmap = ((BitmapDrawable) colorSpectrum.getDrawable()).getBitmap();
+            colorSpectrum.setOnTouchListener(onSpectrumTouched(colorSpectrum, button, bitmap));
+
+            button.setOnClickListener(onColorSelected());
+        }
+    }
+
+    private View.OnClickListener onColorSelected() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateBackgroundColor(mCurrentColor);
+                mRulerView.animateAccentColor(mCurrentColor);
+                mColorDialog.cancel();
+                saveColorSelection();
+            }
+        };
+    }
+
+    private View.OnTouchListener onSpectrumTouched(final ImageView colorSpectrum,
+                                                   final ImageView button, final Bitmap bitmap) {
+        return new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE:
+                        int pixel = getSelectedPixel(event, colorSpectrum, bitmap);
+                        if (pixel != 0) {
+                            mCurrentColor = Color.argb(255,
+                                    Color.red(pixel), Color.green(pixel), Color.blue(pixel));
+                            button.getBackground()
+                                    .setColorFilter(mCurrentColor, PorterDuff.Mode.SRC_ATOP);
+                        }
+                }
+                return true;
+            }
+        };
+    }
+
+    private int getSelectedPixel(MotionEvent event,
+                                 final ImageView colorSpectrum, final Bitmap bitmap) {
+        Matrix inverse = new Matrix();
+        colorSpectrum.getImageMatrix().invert(inverse);
+        float[] touchPoint = new float[]{event.getX(), event.getY()};
+        inverse.mapPoints(touchPoint);
+        int currentX = (int) touchPoint[0];
+        int currentY = (int) touchPoint[1];
+
+        if (currentX < 0) {
+            currentX = 0;
+        }
+        if (currentX > bitmap.getWidth() - 1) {
+            currentX = bitmap.getWidth() - 1;
+        }
+
+        if (currentY < 0) {
+            currentY = 0;
+        }
+        if (currentY > bitmap.getHeight() - 1) {
+            currentY = bitmap.getHeight() - 1;
+        }
+        return bitmap.getPixel(currentX, currentY);
+    }
+
+    private void saveColorSelection() {
+        SharedPreferences.Editor editor = mPrefs.edit();
+        editor.putInt(getString(R.string.ruler_color_pref_key), mCurrentColor);
+        editor.apply();
+    }
 }
